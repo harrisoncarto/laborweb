6f7bc7eef7a3679ab4d98757a62541e9a9623533
Harrison Cole <hcarltoncole@gmail.com>
2020-05-17 22:10:50 -0400

    zooms

--- map.js
+++ map.js
@@ -5,10 +5,6 @@ var geojsonMarkerOptions = {
 	fillOpacity: .5
 };
 
-setTimeout(function () {
-    map.invalidateSize();
-}, 1000);
-
 var map = L.map('map', {
 	center: [38.0902, -95.7129],
 	zoom: 5
@@ -17,7 +13,6 @@ var map = L.map('map', {
 var Stadia_AlidadeSmoothDark = L.tileLayer(
 	'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 		maxZoom: 20,
-		minZoom: 4,
 		attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
