bf902ef1c36f9616c6f65e2a77f10d6429d02469
Harrison Cole <hcarltoncole@gmail.com>
2020-05-17 22:24:06 -0400

    OSM

--- map.js
+++ map.js
@@ -10,10 +10,10 @@ var map = L.map('map', {
 	zoom: 5
 });
 
-var Stadia_AlidadeSmoothDark = L.tileLayer(
-	'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-		maxZoom: 20,
-		attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var OpenStreetMap_Mapnik = L.tileLayer(
+	'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
 var sites;
