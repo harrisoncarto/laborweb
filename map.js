ca7b824adde0128b5b1ee65493f814cc223e35f6
Harrison Cole <hcarltoncole@gmail.com>
2020-05-17 22:39:28 -0400

    stamen

--- map.js
+++ map.js
@@ -10,10 +10,13 @@ var map = L.map('map', {
 	zoom: 5
 });
 
-var OpenStreetMap_Mapnik = L.tileLayer(
-	'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+var Stamen_Toner = L.tileLayer(
+	'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		subdomains: 'abcd',
+		minZoom: 0,
+		maxZoom: 20,
+		ext: 'png'
 	}).addTo(map);
 
 var sites;
