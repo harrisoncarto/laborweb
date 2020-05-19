76111fd66658887c71802fb28ad7b4f583f28245
Harrison Cole <hcarltoncole@gmail.com>
2020-05-18 20:49:04 -0400

    finally mapbox works

--- map.js
+++ map.js
@@ -4,16 +4,33 @@ var geojsonMarkerOptions = {
 	weight: 0,
 	fillOpacity: .5
 };
-
+/*
 var map = L.map('map', {
 	center: [38.0902, -95.7129],
 	zoom: 5
 });
 
+
+
 L.tileLayer(
 	'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+	}).addTo(map);
+	*/
+
+var token = 'pk.eyJ1IjoiaGFpcnlzdW5jb2xkIiwiYSI6ImNrYWQwNm9qeDA1aTIydHFwM2QxNGRjc2sifQ.EjgPn6tT5lAdWE9swlxp7w';
+
+var map = L.map('map', {
+	center: [38.0902, -95.7129],
+	zoom: 5,
+});
+
+L.tileLayer(
+	'https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=' + token, {
+		attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		tileSize: 512,
+		zoomOffset: -1
 	}).addTo(map);
 
 var sites;
